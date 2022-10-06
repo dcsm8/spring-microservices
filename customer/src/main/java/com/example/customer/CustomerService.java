0796e7eebf79972b6b2b44c1c8cb46ab6b127d47
David Sánchez <deividsanchez96@hotmail.com>
2022-10-05 21:10:00 -0500

    Create customer service

---
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -0,0 +1,11 @@
+package com.example.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+
+    }
+}
