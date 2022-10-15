ff87cfbcb917db2874f14239946e545e7a2079c0
David Sánchez <deividsanchez96@hotmail.com>
2022-10-15 00:28:28 -0500

    Create notification microservice

---
+++ notification/src/main/java/com/amigoscode/notification/NotificationService.java
@@ -0,0 +1,26 @@
+package com.amigoscode.notification;
+
+import com.amigoscode.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    public void send(NotificationRequest notificationRequest) {
+        notificationRepository.save(
+                Notification.builder()
+                        .toCustomerId(notificationRequest.getToCustomerId())
+                        .toCustomerEmail(notificationRequest.getToCustomerName())
+                        .sender("Amigoscode")
+                        .message(notificationRequest.getMessage())
+                        .sentAt(LocalDateTime.now())
+                        .build()
+        );
+    }
+}
