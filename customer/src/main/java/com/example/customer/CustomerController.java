9c01b3d8f6d54ad255586299d50045ce54f11fa9
David Sánchez <deividsanchez96@hotmail.com>
2022-10-15 00:47:28 -0500

    Add spring cloud sleuth dependency

--- customer/src/main/java/com/example/customer/CustomerController.java
+++ customer/src/main/java/com/example/customer/CustomerController.java
@@ -1,11 +1,13 @@
 package com.example.customer;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/customers")
 @AllArgsConstructor
@@ -14,6 +16,7 @@ public class CustomerController {
 
     @PostMapping("/")
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+        log.info("New customer registration... {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
     }
 }
