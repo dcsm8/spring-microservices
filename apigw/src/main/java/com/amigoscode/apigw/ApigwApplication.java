dc2efa926e0cdaa9b573924559c982b75c5bea7e
David Sánchez <deividsanchez96@hotmail.com>
2022-10-16 17:34:42 -0500

    Create api gateway microservice

---
+++ apigw/src/main/java/com/amigoscode/apigw/ApigwApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApigwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApigwApplication.class, args);
+    }
+}
